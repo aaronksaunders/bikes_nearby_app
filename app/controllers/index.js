a0c54a163e0187c71cedca955c8c19084b82480f
Aaron Saunders <aaron@clearlyinnovative.com>
2014-06-01 19:06:24 -0400

    moving the app closer to how an appcelerator app should be structured

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,9 +1,21 @@
-var activateStation, drawPath, dropMarker, focusStation, initialize, locate_bikes, myLocation, nearestStations, zoomToFit;
+var drawPath, dropMarker, focusStation, initialize, locate_bikes, myLocation, nearestStations, zoomToFit;
 
+/* library for getting bike information */
+locate_bikes = require('locate_bikes').bikes;
+
+/* closest stations based on current user location */
+nearestStations = [];
+
+/* current user location */
+myLocation = null;
+
+/**
+ *
+ */
 focusStation = function(event) {
   var station, _i, _len, _ref;
   _ref = $.stations.children;
-  for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+  for ( _i = 0, _len = _ref.length; _i < _len; _i++) {
     station = _ref[_i];
     station.setOpacity(0.5);
   }
@@ -14,18 +26,18 @@ focusStation = function(event) {
 zoomToFit = function(map, locations) {
   var delta, lgDiff, location, ltDiff, maxLati, maxLongi, minLati, minLongi, total_locations, _i, _len;
   total_locations = locations.length;
-  for (_i = 0, _len = locations.length; _i < _len; _i++) {
+  for ( _i = 0, _len = locations.length; _i < _len; _i++) {
     location = locations[_i];
-    if ((typeof minLati === "undefined" || minLati === null) || minLati > location.latitude) {
+    if (( typeof minLati === "undefined" || minLati === null) || minLati > location.latitude) {
       minLati = location.latitude;
     }
-    if ((typeof minLongi === "undefined" || minLongi === null) || minLongi > location.longitude) {
+    if (( typeof minLongi === "undefined" || minLongi === null) || minLongi > location.longitude) {
       minLongi = location.longitude;
     }
-    if ((typeof maxLati === "undefined" || maxLati === null) || maxLati < location.latitude) {
+    if (( typeof maxLati === "undefined" || maxLati === null) || maxLati < location.latitude) {
       maxLati = location.latitude;
     }
-    if ((typeof maxLongi === "undefined" || maxLongi === null) || maxLongi < location.longitude) {
+    if (( typeof maxLongi === "undefined" || maxLongi === null) || maxLongi < location.longitude) {
       maxLongi = location.longitude;
     }
   }
@@ -34,72 +46,146 @@ zoomToFit = function(map, locations) {
   delta = ltDiff > lgDiff ? ltDiff : lgDiff;
   if (total_locations > 0 && delta > 0) {
     return map.setLocation({
-      animate: true,
-      latitude: (maxLati + minLati) / 2,
-      longitude: (maxLongi + minLongi) / 2,
-      latitudeDelta: delta * 2,
-      longitudeDelta: delta * 2
+      animate : true,
+      latitude : (maxLati + minLati) / 2,
+      longitude : (maxLongi + minLongi) / 2,
+      latitudeDelta : delta * 2,
+      longitudeDelta : delta * 2
     });
   }
 };
 
-dropMarker = function(station) {
+/**
+ *
+ */
+function dropMarker(station) {
   $.mapview.removeAllAnnotations();
-  station = Alloy.Globals.Map.createAnnotation({
-    latitude: station.latitude,
-    longitude: station.longitude,
-    title: station.stationName,
-    subtitle: station.availableBikes + ' bikes; ' + station.availableDocks + ' docks',
-    pincolor: Alloy.Globals.Map.ANNOTATION_RED,
-    myid: 1
+  var stationAnnotation = Alloy.Globals.Map.createAnnotation({
+    latitude : station.latitude,
+    longitude : station.longitude,
+    title : station.stationName,
+    subtitle : station.availableBikes + ' bikes; ' + station.availableDocks + ' docks',
+    pincolor : Alloy.Globals.Map.ANNOTATION_RED,
+    myid : 1
   });
-  return $.mapview.addAnnotation(station);
+  $.mapview.addAnnotation(stationAnnotation);
 };
 
-drawPath = function(station) {
+/**
+ * @param {Object} station
+ */
+function drawPath(station) {
   var route, routePts;
   routePts = {
-    points: [station, myLocation.coords],
-    color: "blue",
-    width: 4
+    points : [station, myLocation.coords],
+    color : "blue",
+    width : 4
   };
   route = Alloy.Globals.Map.createRoute(routePts);
-  return $.mapview.addRoute(route);
+  $.mapview.addRoute(route);
 };
 
-activateStation = function(station, index) {
+/**
+ *
+ * @param {Object} station
+ * @param {Object} index
+ */
+function activateStation(station, index) {
   dropMarker(station);
-  return zoomToFit($.mapview, [myLocation.coords, station]);
+  return zoomToFit($.mapview, [myLocation, station]);
 };
 
-locate_bikes = require('locate_bikes').bikes;
+/**
+ * when station is clicked, hightlight and have the map drop
+ * a pin and zoom to location
+ */
+$.stationList.addEventListener('itemclick', function(e) {
+  var item = e.section.getItemAt(e.itemIndex);
+  activateStation(item.stationData, e.itemIndex);
+});
 
-nearestStations = [];
+/**
+ * append the data to the list view
+ *
+ * @param {Array} _data - Array of station data
+ */
+function createListView(_data) {
 
-myLocation = null;
+  // clear the list
+  $.section.deleteItemsAt(0, $.section.items.length);
 
-initialize = function() {
+  // this is pretty straight forward, assigning the values to the specific
+  // properties in the template we defined above
+  var items = [];
+  for (var i in _data) {
+
+    // add items to an array
+    items.push({
+      template : 'template1', // set the template
+      stationData : _data[i],
+      numBikes : {
+        text : _data[i].availableBikes
+      },
+      stationName : {
+        text : _data[i].stationName
+      },
+      eta : {
+        text : 'Missing'
+        // the data
+      }
+    });
+  }
+
+  // add the array, items, to the section defined in the view.xml file
+  $.section.setItems(items);
+
+}
+
+function initialize() {
   Ti.Geolocation.accuracy = Ti.Geolocation.ACCURACY_BEST;
   Ti.Geolocation.purpose = "Find the bikes nearest to you";
-  return Ti.Geolocation.getCurrentPosition(function(location) {
-    this.location = location;
-    myLocation = this.location;
-    Ti.API.info(JSON.stringify(this.location));
-    return locate_bikes.fetchBikesNear(this.location, (function(_this) {
-      return function(closestStations) {
-        var index, station, thisStation, _i, _len, _ref;
-        nearestStations = closestStations;
-        _ref = $.stations.children;
-        for (index = _i = 0, _len = _ref.length; _i < _len; index = _i += 2) {
-          station = _ref[index];
-          thisStation = nearestStations[index / 2];
-          station.children[0].text = thisStation.availableBikes;
-          station.children[1].text = thisStation.stationName;
-        }
-        return $.firstStation.fireEvent('click');
-      };
-    })(this));
+
+  Ti.Geolocation.getCurrentPosition(function(location) {
+
+    locate_bikes.fetchBikesNear(location, function(closestStations, coords) {
+
+      // added in for debug purposes
+      myLocation = location = coords;
+      Ti.API.info(JSON.stringify(myLocation));
+
+      if (closestStations.length !== 0) {
+        createListView(closestStations);
+
+        // get the first item, selectItem
+        OS_IOS && $.stationList.selectItem(0, 0);
+        activateStation(closestStations[0], 0);
+
+      } else {
+        alert("Nada!!");
+      }
+    });
   });
+}
+
+function doOpen() {
+  if (OS_ANDROID) {
+    var activity = $.getView().activity;
+    var actionBar = activity.actionBar;
+
+    activity.onCreateOptionsMenu = function(_event) {
+
+      if (actionBar) {
+        actionBar.displayHomeAsUp = true;
+        actionBar.title = 'Bike Finder App';
+        actionBar.onHomeIconItemSelected = function() {
+          $.getView().close();
+        };
+      } else {
+        alert('No Action Bar Found');
+      }
+
+    };
+  }
 };
 
 Ti.App.addEventListener('resumed', function() {
@@ -110,8 +196,10 @@ setInterval(function() {
   return initialize();
 }, 1000 * 60);
 
+// FOR DEBUGGING
+locate_bikes.setDebugLocation([40.70122128, -74.01234218]);
 initialize();
 
-$.index.open();
+$.getView().open();
 
 //# sourceMappingURL=index.js.map
--- app/lib/locate_bikes.js
+++ app/lib/locate_bikes.js
@@ -1,104 +1,127 @@
-(function(exports) {
-  var bikes, distance;
-  distance = require('distance').distance;
-  bikes = {
-    bikeShares: [
-      {
-        city: 'New York City',
-        url: 'http://www.citibikenyc.com/stations/json',
-        latitude: 40.7127,
-        longitude: -74.0059
-      }, {
-        city: 'Chicago',
-        url: 'http://www.divvybikes.com/stations/json',
-        latitude: 41.8819,
-        longitude: -87.6278
-      }, {
-        city: 'San Francisco',
-        url: 'https://www.bayareabikeshare.com/stations/json',
-        latitude: 37.7833,
-        longitude: -122.4167
-      }, {
-        city: 'Columbus',
-        url: 'http://www.cogobikeshare.com/stations/json',
-        latitude: 39.9833,
-        longitude: -82.9833
-      }, {
-        city: 'Aspen',
-        url: 'https://www.we-cycle.org/pbsc/stations.php',
-        latitude: 39.1922,
-        longitude: -106.8244
-      }, {
-        city: 'Chattanooga',
-        url: 'http://www.bikechattanooga.com/stations/json',
-        latitude: 35.0456,
-        longitude: -85.2672
-      }
-    ],
-    simpleDistance: function(coords, station) {
-      return Math.abs(coords.latitude - station.latitude) + Math.abs(coords.longitude - station.longitude);
-    },
-    findNearestStation: function(coords) {
-      var bikeJSON, client;
-      this.bikeShares.sort((function(_this) {
-        return function(city1, city2) {
-          return _this.simpleDistance(coords, city1) - _this.simpleDistance(coords, city2);
-        };
-      })(this));
-      bikeJSON = this.bikeShares[0].url;
-      Ti.API.info(bikeJSON);
-      client = Ti.Network.createHTTPClient({
-        onload: (function(_this) {
-          return function(e) {
-            var bikeStations, closestStations, data, i, station, _i;
-            data = JSON.parse(e.source.responseText);
-            bikeStations = data.stationBeanList;
-            bikeStations.sort(function(station1, station2) {
-              return _this.simpleDistance(coords, station1) - _this.simpleDistance(coords, station2);
-            });
-            closestStations = [];
-            if (distance.getDistance(bikeStations[0], coords) > 48280) {
-              Ti.API.info('too far away');
-              closestStations = false;
-            } else {
-              for (i = _i = 0; _i <= 4; i = ++_i) {
-                station = bikeStations[i];
-                station.distanceInMiles = distance.metersToMiles(distance.getDistance(station, coords));
-                closestStations.push(station);
-              }
-            }
-            if (_this.callback != null) {
-              return _this.callback(closestStations, coords);
-            }
-          };
-        })(this),
-        onerror: (function(_this) {
-          return function(e) {
-            Ti.API.debug(e.error);
-            return alert('error');
-          };
-        })(this),
-        timeout: 5000
-      });
-      client.open("GET", bikeJSON);
-      return client.send();
-    },
-    fetchBikesNear: function(position, callback) {
-      if (callback != null) {
-        this.callback = callback;
-      }
-      return bikes = this.findNearestStation(position.coords);
-    },
-    getBikeData: function(callback) {
-      this.callback = callback;
-      return navigator.geolocation.getCurrentPosition((function(_this) {
-        return function(position) {
-          return _this.fetchBikesNear(position);
-        };
-      })(this));
-    }
-  };
-  return exports.bikes = bikes;
-})(typeof exports === 'undefined' ? this : exports);
+var bikes, distance, DEBUG_LOCATION;
+distance = require('distance').distance;
+exports.bikes = {
+  bikeShares : [{
+    city : 'New York City',
+    url : 'http://www.citibikenyc.com/stations/json',
+    latitude : 40.7127,
+    longitude : -74.0059
+  }, {
+    city : 'Chicago',
+    url : 'http://www.divvybikes.com/stations/json',
+    latitude : 41.8819,
+    longitude : -87.6278
+  }, {
+    city : 'San Francisco',
+    url : 'https://www.bayareabikeshare.com/stations/json',
+    latitude : 37.7833,
+    longitude : -122.4167
+  }, {
+    city : 'Columbus',
+    url : 'http://www.cogobikeshare.com/stations/json',
+    latitude : 39.9833,
+    longitude : -82.9833
+  }, {
+    city : 'Aspen',
+    url : 'https://www.we-cycle.org/pbsc/stations.php',
+    latitude : 39.1922,
+    longitude : -106.8244
+  }, {
+    city : 'Chattanooga',
+    url : 'http://www.bikechattanooga.com/stations/json',
+    latitude : 35.0456,
+    longitude : -85.2672
+  }],
+  /**
+   * used to set default if you are too far away from actual locations
+   */
+  setDebugLocation : function(_coords) {
+    DEBUG_LOCATION = {
+      latitude : _coords[0],
+      longitude : _coords[1],
+    };
+  },
+  /**
+   *
+   * @param {Object} coords
+   * @param {Object} station
+   */
+  simpleDistance : function(coords, station) {
+    return Math.abs(coords.latitude - station.latitude) + Math.abs(coords.longitude - station.longitude);
+  },
+  /**
+   *
+   * @param {Object} coords
+   */
+  findNearestStation : function(coords, callback) {
+    var bikeJSON, client;
+    var that = this;
+
+    SORTED_BIKES = that.bikeShares.sort(function(city1, city2) {
+      return that.simpleDistance(coords, city1) - that.simpleDistance(coords, city2);
+    });
+
+    bikeJSON = SORTED_BIKES[0].url;
+    Ti.API.info(bikeJSON);
+
+    var client = Ti.Network.createHTTPClient({
+      onload : function(e) {
+        var bikeStations, closestStations, data, i, station, _i;
+        data = JSON.parse(client.responseText);
+        bikeStations = data.stationBeanList;
+        bikeStations.sort(function(station1, station2) {
+          return that.simpleDistance(coords, station1) - that.simpleDistance(coords, station2);
+        });
+        closestStations = [];
+        if (distance.getDistance(bikeStations[0], coords) > 48280) {
+          Ti.API.info('too far away');
+          closestStations = [];
+
+          if (DEBUG_LOCATION) {
+            coords = DEBUG_LOCATION;
+          } else {
+            callback([], coords);
+          }
+        }
+
+        for ( i = _i = 0; _i <= 4; i = ++_i) {
+          station = bikeStations[i];
+          station.distanceInMiles = distance.metersToMiles(distance.getDistance(station, coords));
+          closestStations.push(station);
+        }
+
+        if (callback != null) {
+          callback(closestStations, coords);
+        }
+      },
+      onerror : function(e) {
+        Ti.API.debug(e.error);
+        alert('error ' + e.error);
+        callback([], coords);
+      },
+      timeout : 5000
+    });
+    client.open("GET", bikeJSON);
+
+    client.send();
+  },
+  /**
+   *
+   * @param {Object} position
+   * @param {Object} callback
+   */
+  fetchBikesNear : function(position, callback) {
+    this.findNearestStation(position.coords, callback);
+  },
+  /**
+   *
+   * @param {Object} callback
+   */
+  getBikeData : function(callback) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      return exports.fetchBikesNear(position);
+    });
+  }
+};
 
 //# sourceMappingURL=locate_bikes.js.map
